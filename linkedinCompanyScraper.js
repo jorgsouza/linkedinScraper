47febf02193c9fd423a00e8f7ffc5e9b64e10057
Jorge Souza <jorge@vieirasouza.com.br>
2023-01-28 11:17:31 -0300

    Update linkedinCompanyScraper.js

--- linkedinCompanyScraper.js
+++ linkedinCompanyScraper.js
@@ -1,7 +1,7 @@
 const Lnkdn = require("linkedin-profile-company-scraper");
 
 const lkdCookie =
-  "AQEDARXlqNUBJwYvAAABfGFn0sUAAAF9OaSSYU0AIAnp50qJ6Z2WEZMRpDHEeXaMnki8OFKkulioF5JEDuqR4kxxMP3wa3RVdpBhrin1TKbdmsEb7ymDYrGvcAdVJiCLNAif0Zv8JFrJMoTgjW-4spJR";
+  "<Cookie>";
 const perfilProfissional = "https://www.linkedin.com/company/pipelinecapital/";
 
 (async () => {
